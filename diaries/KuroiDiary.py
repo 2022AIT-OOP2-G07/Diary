ccdcc5090098d0dcb3c8cdd6474ada376dbadbe5
DDKur <re9610923@gmail.com>
2022-12-15 12:43:54 +0900

    diary3

--- diaries/KuroiDiary.py
+++ diaries/KuroiDiary.py
@@ -1,13 +1,13 @@
 from diaries.AbstractDiary import AbstractDiary
 
 
-class KuroiDiary(AbstractDiary):
+class KuroiDiary2(AbstractDiary):
 
     def get_date(self):
         return "2022-12-10"
 
     def get_summary(self):
-        return "okasi"
+        return "niku"
 
     def get_author(self):
         return "tabetai"
